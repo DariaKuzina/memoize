65fc3f22f20a89688b41f924d17314c672552820
Daria Kuzina <daria_kuzina@epam.com>
2017-07-10 15:31:13 +0300

    implement memorize

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,20 @@
 export { memoize };
 
 function memoize(func) {
-	// TODO
+	var _cashe = new Map();
+	return function () {
+		var key = getKey(arguments);
+		if (!_cashe.has(key)) {
+			_cashe.set(key, func.apply(null, arguments));
+		}
+		return _cashe.get(key);
+	}
+}
+
+function getKey(args) {
+	var res = "";
+	for (var i = 0; i < args.length; i++) {
+		res += args[i] + typeof (args[i]) + ";";
+	}
+	return res;
 }
